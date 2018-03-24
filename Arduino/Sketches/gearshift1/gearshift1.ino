0eb8b18e1e638df1295b0d824fa1344866edbb57
Will Atherton <williamdatherton@gmail.com>
2018-03-24 20:56:11 +0000

    Delays
    Delays for automatically closing the valves have been added.

--- Arduino/Sketches/gearshift1/gearshift1.ino
+++ Arduino/Sketches/gearshift1/gearshift1.ino
@@ -6,15 +6,22 @@ int downPin =6;
 int upOut = 12;     // pushbutton connected to digital pin 7
 int downOut =11;
 
-int valup = 0;       // variable to store the read value
-int valdown = 0;       // variable to store the read value
+int Inputvalup = 0;       // variable to store the read value
+int Inputvaldown = 0;       // variable to store the read value
 
+int uptime =1000;
+int upcount =0;
+
+int downtime =1000;
+int downcount =0;
+
+bool upReq =false;
+bool downReq =false;
 
 
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);      // sets the digital pin 13 as output
 
   pinMode(upOut, OUTPUT);        // sets the digital pin 7 as input
   pinMode(downOut, OUTPUT);        // sets the digital pin 7 as input
@@ -32,15 +39,47 @@ void setup()
 
 void loop()
 {
-  valup = !digitalRead(upPin);     // read the input pin
-  digitalWrite(ledPin, valup);    // sets the LED to the button's value
-  digitalWrite(upOut, valup);    // sets the LED to the button's value
-  Serial.print(digitalRead(upOut));
+  Inputvalup = !digitalRead(upPin);     // read the input pin
+  Inputvaldown = !digitalRead(downPin);     // read the input pin
 
-  valdown = !digitalRead(downPin);     // read the input pin
-  digitalWrite(ledPin, valdown);    // sets the LED to the button's value
-  digitalWrite(downOut, valdown);    // sets the LED to the button's value
-  Serial.print(digitalRead(downOut));
-  Serial.println();
-//  Serial.printf ("down gear= %d, up gear =%d\n", valup, valdown);
+  if(Inputvalup ==1){
+    upReq =true;
+  }
+
+  if(Inputvaldown ==1){
+    downReq =true;
+  }
+
+  if(upReq){
+    if(upcount<uptime){
+      digitalWrite(upOut, 1);    //open valve
+      Serial.println("up 1 ");
+    
+    }else{
+
+      digitalWrite(upOut, 0);    //open valve
+      Serial.println("up 0 ");
+      upReq =false;
+      upcount=0;
+    }
+  upcount++;
+    
+  }
+
+  if(downReq){
+    if(downcount<downtime){
+      digitalWrite(downOut, 1);    //open valve
+      Serial.println("down 1");
+    
+    }else{
+
+      digitalWrite(downOut, 0);    //open valve
+      Serial.println("down 0");
+      downReq =false;
+      downcount=0;
+    }
+  downcount++;
+    
+  }
+  
 }
